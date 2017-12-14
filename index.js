e18b8d7ffebdc0697df82278f3cd401fc1c04a1c
David Perez <davperez89@gmail.com>
2017-12-14 19:44:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = {key:"value"};
 
 function updateObjectWithKeyAndValue(object, key, value){
-  Object.Assign({},object, [key], value})
+  Object.Assign({},object, {[key], value})
   return recipes;
 }
 
