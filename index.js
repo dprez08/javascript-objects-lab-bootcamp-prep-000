6248ead8b3d5e6be2d35056836de98119e2d6f78
David Perez <davperez89@gmail.com>
2017-12-14 19:54:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({},object, {[key]: value});
-  
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object,key, value){
-  object = value;
+  object[key] = value;
   return object;
 }
