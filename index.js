91178b0865f19bc9e8bf945a30f459146ea9fc7a
David Perez <davperez89@gmail.com>
2017-12-14 19:58:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object,key, value){
 }
 
 function deleteFromObjectByKey(object,key){
-  delete object[value] = key;
+  delete object[value];
   return object;
 }
